95a2da7bd6e2f59499f201e532dc3e20017dd1ab
Valentina Otočan <valentinaotocan@gmail.com>
2023-04-27 12:04:55 +0200

    Add BrowserRouter in main.tsx and Home route in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
   );
 }
 export default App;
