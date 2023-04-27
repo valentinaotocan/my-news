75d7e8e0dde14afb882712f2bbacd05f13d21102
Valentina Otočan <valentinaotocan@gmail.com>
2023-04-27 21:39:29 +0200

    Tsx for Layout, Search, SearchResults and routes for them

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { SearchContextProvider } from "./components/context/SearchContext";
 import App from "./App";
 import "./styles/main.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <SearchContextProvider>
+        <App />
+      </SearchContextProvider>
     </React.StrictMode>
   </BrowserRouter>
 );
