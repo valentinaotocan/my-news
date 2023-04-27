6164a382dffde41091d1194059c6958b5b70bd24
Valentina Otočan <valentinaotocan@gmail.com>
2023-04-27 17:13:55 +0200

    Category (route, tsx, style for desktop and mobile)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Category from "./pages/Category";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path=":categoryId" element={<Category />} />
       </Routes>
     </div>
   );
---
+++ src/pages/Category.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Article } from "../types";
+import Spinner from "../components/Spinner";
+import Error from "../components/Error";
+import Card from "../components/Card";
+
+function Category() {
+  const { categoryId } = useParams();
+  const [categoryArticles, setCategoryArticles] = useState<Article[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const apiKey = import.meta.env.VITE_API_KEY;
+
+  useEffect(() => {
+    const fetchCategoryArticles = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(
+          `https://newsapi.org/v2/top-headlines?category=${categoryId}&language=en&sortBy=publishedAt&apiKey=${apiKey}`
+        );
+        if (response.ok) {
+          const data = await response.json();
+          setCategoryArticles(data.articles);
+          setError(false);
+        } else {
+          setError(true);
+        }
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchCategoryArticles();
+  }, [categoryId]);
+
+  return (
+    <div className="pl-small">
+      <h2 className="news-title inter-v inter-v--semi">{categoryId}</h2>
+      <div className="cards">
+        {loading && <Spinner />}
+        {error && <Error />}
+        {categoryArticles.map((article, index) => (
+          <Card key={index} article={article} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Category;
