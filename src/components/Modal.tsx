1eed71c7cd8afaddd88c70433e5519f3484667b1
Valentina Otočan <valentinaotocan@gmail.com>
2023-04-27 17:29:56 +0200

    Modal.tsx

---
+++ src/components/Modal.tsx
@@ -0,0 +1,18 @@
+import shapeLine from "../assets/images/shape-line.png";
+
+function Modal() {
+  return (
+    <div className="modal app-padding clr-white">
+      <h2 className="modal__heading inter-v inter-v--bold">
+        Make MyNews your homepage
+      </h2>
+      <p className="modal__paragraph inter-v">
+        Every day discover what's trending on the internet!
+      </p>
+      <img src={shapeLine} alt="Shape Line" />
+      <button className="btn btn--white inter-v inter-v--bold">Get</button>
+      <p className="modal__no-thanks inter-v inter-v--bold">No, thanks</p>
+    </div>
+  );
+}
+export default Modal;
