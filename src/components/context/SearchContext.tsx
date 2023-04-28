ddf0cb51f85a1562438d3bd145aa00fcf046dcae
Valentina Otočan <valentinaotocan@gmail.com>
2023-04-28 11:35:31 +0200

    Rename SearchContextProviderType to ContextProviderType

--- src/components/context/SearchContext.tsx
+++ src/components/context/SearchContext.tsx
@@ -1,9 +1,5 @@
 import { createContext, useState } from "react";
-import {
-  Article,
-  SearchContextType,
-  SearchContextProviderType,
-} from "../../types";
+import { Article, SearchContextType, ContextProviderType } from "../../types";
 
 export const SearchContext = createContext<SearchContextType>({
   searchedResults: [],
@@ -14,9 +10,7 @@ export const SearchContext = createContext<SearchContextType>({
   setError: () => {},
 });
 
-export const SearchContextProvider = ({
-  children,
-}: SearchContextProviderType) => {
+export const SearchContextProvider = ({ children }: ContextProviderType) => {
   const [searchedResults, setSearchedResults] = useState<Article[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
