c872aedf6b6930e088dbef084ce5c7ef4478d3c6
Valentina Otočan <valentinaotocan@gmail.com>
2023-05-08 19:27:06 +0200

    Sort favorites articles from new to old in Favorites page

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -5,13 +5,18 @@ import Card from "../components/Card";
 function Favorites() {
   const { favorites } = useContext(FavoritesContext);
 
+  const sortedFavoritesFromNew = [...favorites].sort(
+    (a, b) =>
+      new Date(b.publishedAt).getTime() - new Date(a.publishedAt).getTime()
+  );
+
   return (
     <>
       <div className="pl-small w-100">
         <h2 className="news-title inter-v inter-v--semi">My Favorites</h2>
         <div className="cards">
-          {favorites.length > 0 ? (
-            favorites.map((article, index) => (
+          {sortedFavoritesFromNew.length > 0 ? (
+            sortedFavoritesFromNew.map((article, index) => (
               <Card key={index} article={article} />
             ))
           ) : (
